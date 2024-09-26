63dab0da90435d01846defd489f87ca593f6d592
Muzadded Chowdhury <alifchowdhury27@gmail.com>
2024-09-26 16:53:42 +0600

    Update Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Intervention\Image\Laravel\Facades\Image;
 use Illuminate\Support\Facades\DB;
+use RealRashid\SweetAlert\Facades\Alert;
+
 
 class CategoryController extends Controller
 {
@@ -80,7 +82,7 @@ class CategoryController extends Controller
             }
         }
 
-        //Alert::success('Category Created Successfully!', 'success');
+        Alert::success('Category Created Successfully!', 'success');
         return Redirect::to('/admin/category');
     }
 
@@ -108,7 +110,79 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $category_model = new Category();
+
+        $category_model = $category_model->find($request->category_row_id); // edit operation.
+        $category_model->category_name = $request->category_name;  
+        $category_model->parent_id = $request->parent_id;
+
+        // parent changed ? 
+        $parent_id_changed = 0;
+        $prev_parent_id = DB::table('categories')->where('category_row_id', $request->category_row_id)->first()->parent_id;
+        if($request->parent_id != $prev_parent_id) {
+            $parent_id_changed = 1; // just status to understand parent id has been changed
+        }
+
+        // get level,  level = parent level + 1.
+        $category_model->level = 0;
+        if($category_model->parent_id) {
+          // fetching modified parent id main category information
+          $parent_cat_info =   DB::table('categories')->where('category_row_id',$category_model->parent_id)->first(); 
+          $category_model->level = $parent_cat_info->level + 1;
+        }
+
+        if(isset($request->category_image)){
+            $category_image   = $request->file('category_image');
+            $filename         = time().'_'.$category_image->getClientOriginalName();
+
+            $category_image->move(public_path('uploads/category').'/original/',$filename);
+            $image_resize = Image::read(public_path('uploads/category').'/original/'.$filename);
+            $image_resize->resize(200, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+            $image_resize->save(public_path('uploads/category').'/thumbnail/'.$filename);
+            $category_model->category_image = $filename;
+        } else {
+            $prev_category_image = DB::table('categories')->where('category_row_id', $request->category_row_id)->first()->category_image;
+            if($prev_category_image != NULL){
+                $category_model->category_image = $prev_category_image;    
+            } else {
+                $category_model->category_image = null;
+            }
+            
+        }
+
+        $category_model->category_description = $request->long_desc;
+        $category_model->is_featured = $request->is_featured?1:0;
+
+        $category_model->save();
+
+        // update has_child status of present parent         
+        if($category_model->parent_id)
+        {
+           if($parent_cat_info->has_child != 1)
+           { 
+               DB::table('categories')->where('category_row_id', $request->parent_id)
+                ->update([
+                  'has_child'=> 1
+                ]);
+           }
+        }
+
+        // update  has_child status of previous parent 
+        if($parent_id_changed){            
+           $total_child_count = DB::table('categories')->where('parent_id', $prev_parent_id)->count();
+           if($total_child_count == 0)
+           {
+                DB::table('categories')->where('category_row_id', $prev_parent_id)
+                ->update([
+                  'has_child'=> 0
+                ]);
+           }      
+        }
+
+        Alert::success('Category Updated Successfully!', 'success');
+        return redirect()->route('category.index'); 
     }
 
     /**
